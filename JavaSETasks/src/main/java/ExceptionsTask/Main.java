bf57383f6db27ef527a15145e032768eb90bda7e
Danila Verstov <VerstovDI@yandex.ru>
2020-09-09 12:39:18 +0300

    Update SeaBattle, add Exception Task

--- JavaSETasks/src/main/java/ExceptionsTask/Main.java
+++ JavaSETasks/src/main/java/ExceptionsTask/Main.java
@@ -1,23 +1,30 @@
 package ExceptionsTask;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>();
         for (int i = 0; i < 11; i++) {
-            if (arrayList.size() >= 10) {
-                try {
-                    if (arrayList.size() >= 10) {
-                        throw new MyArrayListException("Only 10 elements to hold are possible");
-                    }
-                    arrayList.add(i);
-                } catch (MyArrayListException arrayListException) {
-                    arrayListException.printStackTrace();
-                }
+            try {
+                addToBoundedArray(arrayList, i);
+            } catch (MyArrayListException arrayListException) {
+                Locale.setDefault(Locale.FRANCE);
+                System.err.println(arrayListException.getLocalizedMessage());
+                System.err.println(arrayListException.getMessage());
+                arrayListException.printStackTrace();
             }
-            arrayList.add(i);
         }
     }
+
+    public static void addToBoundedArray(ArrayList<Integer> arrayList,
+                                         Integer i) throws MyArrayListException {
+        if (arrayList.size() < 10) {
+            arrayList.add(i);
+        } else {
+            throw new MyArrayListException("Too many elements in the array!");
+        }
+    }
+
 }
--- JavaSETasks/src/main/java/ExceptionsTask/MyArrayListException.java
+++ JavaSETasks/src/main/java/ExceptionsTask/MyArrayListException.java
@@ -2,12 +2,22 @@ package ExceptionsTask;
 
 import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
+/**
+ * The MyArrayListException wraps standard Java exception to control size of array.
+ * If size of an array more than {@link MyArrayListException#sizeBound}.
+ * @author Verstov Danila
+ * @version 1.0
+ */
 public class MyArrayListException extends Exception {
-    // TODO: реализовать стандартные конструкторы и т.д.
-    // TODO: пока перегружен toString() [1. Осталось 3]
+    private static final long serialVersionUID = 7918828512120319558L;
+    ResourceBundle labels = ResourceBundle.getBundle("text", Locale.FRANCE);
+    /** Field that represents maximum number of elements in the array **/
     private final int sizeBound = 10;
+    private String exceptionMessage;
 
     public MyArrayListException() {
         super();
@@ -15,89 +25,68 @@ public class MyArrayListException extends Exception {
 
     public MyArrayListException(String message) {
         super(message);
+        this.exceptionMessage = message;
     }
 
     public MyArrayListException(String message, Throwable cause) {
         super(message, cause);
+        this.exceptionMessage = message;
     }
 
     public MyArrayListException(Throwable cause) {
         super(cause);
     }
 
-    public MyArrayListException(String message, int size) {
-        super(message);
-        //this.size = size;
-    }
-
     public int getSizeBound() {
         return sizeBound;
     }
 
-    /*public ArrayList<Integer> getBoundedArrayList() {
-        return boundedArrayList;
+    @Override
+    public String toString() {
+        String s = getClass().getName();
+        String message = getLocalizedMessage(); // getMessage()
+        if (message == null) {
+            message = this.exceptionMessage;
+        }
+        return (message != null) ? (s + ": " + message) : s;
     }
 
-    public void setBoundedArrayList(ArrayList<Integer> boundedArrayList) {
-        this.boundedArrayList = boundedArrayList;
-    }*/
 
     @Override
     public String getMessage() {
-        return "Size can't be more than"
-                .concat(String.valueOf(sizeBound))
-                .concat(super.getMessage());
+        if (this.exceptionMessage == null) {
+            return "Can't be more than "
+                    + sizeBound
+                    + " "
+                    + "elements in the array!"
+                    + super.getMessage();
+        } else {
+            return this.exceptionMessage;
+        }
+        /*return this.exceptionMessage
+                .concat("! ")
+                .concat(super.getMessage());*/
     }
 
     @Override
     public String getLocalizedMessage() {
-        return super.getLocalizedMessage();
+        return labels.getString("tooBigSize");
     }
 
-    /*@Override
+    @Override
     public synchronized Throwable getCause() {
+        //return (cause==this ? null : cause);
         return super.getCause();
     }
 
     @Override
     public synchronized Throwable initCause(Throwable cause) {
         return super.initCause(cause);
-    }*/
-
-    @Override
-    public String toString() {
-        return "MyArrayListException{" +
-                "sizeBound=" + sizeBound +
-                '}';
     }
 
     @Override
     public void printStackTrace() {
+        System.err.println(new Date().toString());
         super.printStackTrace();
-    }
-
-    @Override
-    public void printStackTrace(PrintStream s) {
-        super.printStackTrace(s);
-    }
-
-    @Override
-    public void printStackTrace(PrintWriter s) {
-        super.printStackTrace(s);
-    }
-
-    /*@Override
-    public synchronized Throwable fillInStackTrace() {
-        return super.fillInStackTrace();
-    }*/
-
-    @Override
-    public StackTraceElement[] getStackTrace() {
-        return super.getStackTrace();
-    }
-
-    @Override
-    public void setStackTrace(StackTraceElement[] stackTrace) {
-        super.setStackTrace(stackTrace);
     }
 }
