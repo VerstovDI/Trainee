b7bf38f1b678f3658396c404e561e45f564a32cd
Danila Verstov <VerstovDI@yandex.ru>
2020-10-26 18:14:20 +0300

    Step 6 DONE

--- EduApp/src/main/java/ru/greenatom/edu/config/WebSecurityConfig.java
+++ EduApp/src/main/java/ru/greenatom/edu/config/WebSecurityConfig.java
@@ -3,16 +3,16 @@ package ru.greenatom.edu.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import ru.greenatom.edu.service.UserService;
 
-import javax.sql.DataSource;
-
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
 
--- EduApp/src/main/java/ru/greenatom/edu/controller/UserController.java
+++ EduApp/src/main/java/ru/greenatom/edu/controller/UserController.java
@@ -1,18 +1,22 @@
 package ru.greenatom.edu.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.greenatom.edu.domain.Role;
 import ru.greenatom.edu.domain.User;
 import ru.greenatom.edu.repository.UserRepository;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     private UserRepository userRepository;
 
@@ -27,11 +31,34 @@ public class UserController {
         return "userList";
     }
 
-    @GetMapping("{user}")
+    /*
+    можно просто @GetMapping({user})
+    или @GetMapping(value = "{user.id}")
+     */
+    @GetMapping(value = "/{user}")
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
         return "userEdit";
     }
 
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam(value = "userId") User user) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+        return "redirect:/user";
+    }
 }
