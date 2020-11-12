16418e2c0db2038082e7c34f2c4b5c6db12b923d
Danila Verstov <VerstovDI@yandex.ru>
2020-11-12 22:15:50 +0300

    Step 12 DONE

--- EduApp/src/main/java/ru/greenatom/edu/config/WebSecurityConfig.java
+++ EduApp/src/main/java/ru/greenatom/edu/config/WebSecurityConfig.java
@@ -39,6 +39,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .defaultSuccessUrl("/main", true)
                     .permitAll()
                 .and()
+                    .rememberMe()
+                .and()
                     .logout()
                     .permitAll();
     }
--- EduApp/src/main/java/ru/greenatom/edu/controller/RegistrationController.java
+++ EduApp/src/main/java/ru/greenatom/edu/controller/RegistrationController.java
@@ -1,22 +1,36 @@
 package ru.greenatom.edu.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import ru.greenatom.edu.domain.User;
+import ru.greenatom.edu.domain.dto.CaptchaResponseDto;
 import ru.greenatom.edu.service.UserService;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
+    private static final String CAPTCHA_URL =
+            "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
     //@Autowired
     private final UserService userService;
+
+    @Value("${recaptcha.secret}")
+    private String recaptchaSecret;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Autowired
     public RegistrationController(UserService userService) {
@@ -29,12 +43,31 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
-        if (user.getPassword() != null && !user.getPassword().equals(user.getPassword2())) {
-            model.addAttribute("passwordError", "Passwords are different!");
-            return "registration";
+    public String addUser(
+            @RequestParam("password2") String passwordConfirm,
+            @RequestParam("g-recaptcha-response") String captchaResponse,
+            @Valid User user,
+            BindingResult bindingResult,
+            Model model) {
+        String captchaUrl = String.format(CAPTCHA_URL, recaptchaSecret, captchaResponse);
+        CaptchaResponseDto response =
+                restTemplate.postForObject(captchaUrl, Collections.emptyList(), CaptchaResponseDto.class);
+
+        if (!response.isSuccess()) {
+            model.addAttribute("captchaError", "Fill captcha, please!");
         }
-        if (bindingResult.hasErrors()) {
+        boolean isPasswordDifferent =
+                user.getPassword() != null && !user.getPassword().equals(passwordConfirm);
+
+        if(isPasswordDifferent){
+            model.addAttribute("passwordError", "Passwords are different!");
+        }
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+        if (isConfirmEmpty) {
+            model.addAttribute("password2Error", "Password confirmation cancelled!");
+        }
+        if (isConfirmEmpty || bindingResult.hasErrors()
+                || !response.isSuccess() || isPasswordDifferent) {
             Map<String, String> errorsMap = ControllerUtil.getErrors(bindingResult);
             model.mergeAttributes(errorsMap);
             return "registration";
@@ -50,8 +83,10 @@ public class RegistrationController {
     public String activate(Model model, @PathVariable String code) {
         boolean isActivated = userService.activateUser(code);
         if (isActivated) {
+            model.addAttribute("messageType", "success");
             model.addAttribute("message", "User successfully activated!");
         } else {
+            model.addAttribute("messageType", "danger");
             model.addAttribute("message", "Activation code wasn't found!");
         }
         return "login";
