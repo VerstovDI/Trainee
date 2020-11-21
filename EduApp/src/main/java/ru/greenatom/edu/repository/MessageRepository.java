bd1c9e17e5f827bfff4be06e9c2ae26b9a34506f
Danila Verstov <VerstovDI@yandex.ru>
2020-11-21 16:49:01 +0300

    Step 20 (IN_PROGRESS, bugfixes)

--- EduApp/src/main/java/ru/greenatom/edu/repository/MessageRepository.java
+++ EduApp/src/main/java/ru/greenatom/edu/repository/MessageRepository.java
@@ -13,19 +13,19 @@ public interface MessageRepository extends CrudRepository<Message, Long> {
     @Query("select new ru.greenatom.edu.domain.dto.MessageDto(" +
            "    m, " +
             "   count(ml),  " +
-            " sum(case when ml =: user then 1 else 0 end) > 0" +
+            " sum(case when ml = :user then 1 else 0 end) > 0" +
             ") " +
-            "from Message m left join m.likes ml "
-            + "group by m")
+            "from Message m left join m.likes ml " +
+            "group by m")
     Page<MessageDto> findAll(Pageable pageable, @Param("user") User user);
 
     @Query("select new ru.greenatom.edu.domain.dto.MessageDto(" +
             " m, " +
             " count(ml), " +
-            " sum(case when ml =: user then 1 else 0 end) > 0 " +
+            " sum(case when ml = :user then 1 else 0 end) > 0 " +
             ") " +
             " from Message m left join m.likes ml " +
-            "where m.tag =: tag " +
+            "where m.tag = :tag " +
             "group by m")
     Page<MessageDto> findByTag(@Param("tag") String tag, Pageable pageable, @Param("user") User user);
 
